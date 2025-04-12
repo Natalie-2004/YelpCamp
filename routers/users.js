b61d80ca665352f3473e572d3dc7b1d3f8defe84
Natalie Ye <1103625169lixuan@gmail.com>
2025-04-12 13:39:14 +1000

    fix typo

--- routers/users.js
+++ routers/users.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const route = express.Router();
+const router = express.Router();
 const User = require('../models/user.js');
 const catchAsync = require('../utilities/catchAsync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users.js');
 
-route.route('/register')
+router.route('/register')
     .get(users.renderRegister)
     .post(catchAsync(users.registerUser));
 
 // https://www.passportjs.org/packages/passport-local/
 // storeReturnTo saves the returnTo value from session to res.locals,
 // as after login req.session will be clear and now able to use preserved value to redirect
-route.route('/login')
+router.route('/login')
     .get(users.renderLogin)
     .post(storeReturnTo, 
         passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), 
         users.loginUser
     )
 
-route.get('/logout', users.logoutUser);
+router.get('/logout', users.logoutUser);
 
-module.exports = route;
+module.exports = router;
