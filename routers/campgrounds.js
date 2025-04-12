553d821f22871fbb0bb6fa4c6e6e5e3164c83fb1
Natalie Ye <1103625169lixuan@gmail.com>
2025-04-12 13:38:28 +1000

    restructure routes

--- routers/campgrounds.js
+++ routers/campgrounds.js
@@ -4,24 +4,23 @@ const catchAsync = require('../utilities/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 
-// index page
-router.get('/', catchAsync(campgrounds.index));
+// group all the common routes together
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.create))
 
-// new page
+// new page, need to go before show page route
 router.get('/new', isLoggedIn, campgrounds.new);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.create))
-
-// show page -> ':' have the lowest priority at the same route 
-router.get('/:id', catchAsync(campgrounds.show));
+// show page -> ':' have the lowest priority at the same route
+// update database, send whenever the form is submitted
+// delete a specific campground through id searching
+router.route('/:id')
+    .get(catchAsync(campgrounds.show))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.update))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.delete))
 
 // edit page -> find and enter the target page to update
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.edit));
-
-// update database, send whenever the form is submitted
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.update));
-
-// delete a specific campground through id searching
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.delete));
 
 module.exports = router;
--- routers/users.js
+++ routers/users.js
@@ -1,25 +1,25 @@
 const express = require('express');
-const routers = express.Router();
+const route = express.Router();
 const User = require('../models/user.js');
 const catchAsync = require('../utilities/catchAsync');
 const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 const users = require('../controllers/users.js');
 
-routers.get('/register', users.renderRegister)
-
-routers.post('/register', catchAsync(users.registerUser))
-
-routers.get('/login', users.renderLogin)
+route.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.registerUser));
 
 // https://www.passportjs.org/packages/passport-local/
 // storeReturnTo saves the returnTo value from session to res.locals,
 // as after login req.session will be clear and now able to use preserved value to redirect
-routers.post('/login', storeReturnTo, 
-    passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), 
-    users.loginUser
-);
+route.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, 
+        passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), 
+        users.loginUser
+    )
 
-routers.get('/logout', users.logoutUser);
+route.get('/logout', users.logoutUser);
 
-module.exports = routers;
+module.exports = route;
