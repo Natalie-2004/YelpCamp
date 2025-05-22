f1caf68ead798240be730dd12712abe71339af57
Natalie Ye <1103625169lixuan@gmail.com>
2025-05-22 19:22:40 +1000

    popup camp marker

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,11 +3,17 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 3, // starting zoom
+    zoom: 9, // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset: 25,})
+        .setHTML(
+            `<h2>${campground.title}</h2>`
+        )
+    )
     .addTo(map);
